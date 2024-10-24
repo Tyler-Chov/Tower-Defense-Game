e50e64c6ea2b8949ceaa261dd54050f6abbcf341
Tyler Chov <chovt@mail.gvsu.edu>
2024-10-23 21:36:27 -0400

    Implemented Basic Tower Placement Logic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame, sys, os, button
-
+from tower import Tower
 FPS = 60
 fpsClock = pygame.time.Clock()
 window_width = 800
@@ -44,7 +44,6 @@ class StartScreen:
         return False
 
 
-
 class MainGameScreen:
     def __init__(self, window):
         self.window = window
@@ -62,7 +61,10 @@ class MainGameScreen:
         self.grid_active = False
         self.grid_size = 14
         self.map_path = ((0, 274), (116, 274), (116, 124), (258, 124), (258, 322), (444, 322), (444, 226), (700, 226))
-        
+        self.tower_size = 3
+        self.selected_tower = None
+        self.placed_towers = []
+
         #Debugging Variables
         self.debug = False
         self.cursor_text = self.font.render('', True, (255, 255, 255)) 
@@ -94,20 +96,33 @@ class MainGameScreen:
         Rectangle(705, 300, 90, 90, (100, 100, 100)),
         Rectangle(705, 400, 90, 90, (100, 100, 100))
         ]
+        tower_image = pygame.image.load(os.path.join("game_assests", "tower.png"))
+        tower_image = pygame.transform.scale(tower_image, (90, 90))
 
         for box in tower_boxes:
             box.draw()
         
+        self.window.blit(tower_image, (705, 100))
+        
         if self.grid_active:
             self.draw_grid()
+            self.render_tower_preview()
         
         self.window.blit(self.health_text, (705, 10))
         self.window.blit(self.money_text, (705, 40))
         self.window.blit(self.wave_text, (705, 70))
         
+        for tower in self.placed_towers:
+            tower.render(self.window)
+
         if self.debug:
             self.update_cursor_position()
             self.draw_enemy_path()
+            self.render_tower_preview()
+            '''
+            for tower in self.placed_towers:
+                print(tower._position) 
+            '''
 
         """
         # potential code for pause button hitbox
@@ -134,6 +149,35 @@ class MainGameScreen:
             pygame.draw.line(self.window, grid_color, (x, 0), (x, grid_height))
         for y in range(0, grid_height, self.grid_size):
             pygame.draw.line(self.window, grid_color, (0, y), (grid_width, y))
+        
+
+    def render_tower_preview(self):
+        for tower in self.placed_towers:
+            range_surface = pygame.Surface(
+                (self.tower_size * self.grid_size, self.tower_size * self.grid_size),
+                pygame.SRCALPHA
+            )
+            range_surface.fill((128, 128, 128, 150))
+            self.window.blit(
+                range_surface, 
+                (tower._position[0], tower._position[1])
+            )
+
+        if not self.debug:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            preview_size = self.tower_size * self.grid_size
+            grid_x = ((mouse_x - preview_size // 2) // self.grid_size * self.grid_size)
+            grid_y = ((mouse_y - preview_size // 2) // self.grid_size * self.grid_size)
+            preview_surface = pygame.Surface(
+                (preview_size, preview_size),
+                pygame.SRCALPHA
+            )
+            preview_surface.fill((128, 128, 128, 100))
+            self.window.blit(preview_surface, (grid_x, grid_y))
+            temp_tower = Tower("Archer Tower", 50, 3, 100, 5, 1)
+            temp_tower.place((grid_x, grid_y))
+            temp_tower.render(self.window)
+
 
     def check_for_click(self):
         for event in pygame.event.get():
@@ -150,14 +194,61 @@ class MainGameScreen:
                 ]
                 for box in tower_boxes:
                     if box.collidepoint(mouse_pos):
-                        self.grid_active = not self.grid_active 
+                        self.grid_active = not self.grid_active
                         return
+
+                if self.grid_active:
+                    self.place_tower(mouse_pos)
                     
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_p:
-                    self.debug = True
+                    if self.debug == True:
+                        self.debug = False
+                    else: 
+                        self.debug = True
                     return
+    
+    def place_tower(self, mouse_pos):
+        tower_size_px = self.tower_size * self.grid_size
+        grid_x = (mouse_pos[0] - tower_size_px // 2) // self.grid_size * self.grid_size
+        grid_y = (mouse_pos[1] - tower_size_px // 2) // self.grid_size * self.grid_size
 
+        if self.check_collision(grid_x, grid_y): #I will actually add a proper error in game later, dunno what's the best way to do it.
+            print("Cannot place tower here! It overlaps with another tower's range.")
+            return
+
+        new_tower = Tower("Archer Tower", 50, 3, 100, 5, 1)
+        new_tower.place((grid_x, grid_y))
+        self.placed_towers.append(new_tower)
+        self.selected_tower = None
+        self.grid_active = False
+    
+    def check_collision(self, grid_x, grid_y):
+        new_rect = pygame.Rect(
+            grid_x, 
+            grid_y, 
+            self.grid_size * self.tower_size, 
+            self.grid_size * self.tower_size
+        )
+
+        for tower in self.placed_towers:
+            tower_range = pygame.Rect(
+                tower._position[0], 
+                tower._position[1], 
+                self.grid_size * self.tower_size, 
+                self.grid_size * self.tower_size
+            )
+            if new_rect.colliderect(tower_range):
+                return True
+
+        bottom_bar = pygame.Rect(0, window_height - 100, window_width, 100)
+        side_bar = pygame.Rect(window_width - 100, 0, 100, window_height)
+
+        if new_rect.colliderect(bottom_bar) or new_rect.colliderect(side_bar):
+            return True
+
+        return False
+        
     def remove_health(self, health):
         self.health -= health
         self.health_text = self.font.render(f'Health: {self.health}', True, (255, 255, 255))
