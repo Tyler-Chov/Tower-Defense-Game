0f401bbe7e8914292cc783258a14ac8b519cf36b
yonkerz <yonkerz@mail.gvsu.edu>
2024-10-29 12:41:59 -0400

    Add files via upload

--- enemy.py
+++ enemy.py
@@ -1,70 +1,95 @@
 import pygame.draw
+"""pygame.draw used to more easily render enemies"""
 
 
 class Enemy:
+    """Class that handles the creation of enemies"""
     def __init__(self, e_type, health, speed, strength, path):
         self._type = e_type
+        """The type of enemy, to be used at a later date for different kinds of enemies"""
         self._health = health
         self._max_health = health
         self._speed = speed
         self._strength = strength
         self._path = path
+        """The path the enemy is supposed to follow"""
         self._position = self._path[0]
+        """Initially set at the starting point on the path"""
         self._path_index = 0
-        self._status = True  # Means the enemy is alive
+        self._status = True
+        """Means the enemy is alive"""
         self._resource_worth = health * 3
 
     def _move(self):
+        """Function to move the enemy"""
         if self._path_index < len(self._path) - 1:
+            """Checks to see if the enemy has anywhere else to go."""
             target_x, target_y = self._path[self._path_index]
+            """Finds the target x and y"""
             dir_x, dir_y = target_x - self._position[0], target_y - self._position[1]
+            """Sets the direction to go"""
             distance = (dir_x ** 2 + dir_y ** 2) ** 0.5
+            """Helps adjust distance and move based on speed stat"""
             if distance < self._speed:
                 self._position = (target_x, target_y)
-                self._path_index += 1  # Move to the next point
+                self._path_index += 1
+                """Move to the next point"""
             else:
                 self._position = (
                     self._position[0] + dir_x / distance * self._speed,
                     self._position[1] + dir_y / distance * self._speed,
                 )
 
-    def take_damage(self, damage):
+    def take_damage(self, damage, player):
+        """Takes damage and calls kill_enemy if health hits zero"""
         self._health = self._health - damage
         if self.get_enemy_health() <= 0:
-            self.kill_enemy()
+            self.kill_enemy(player)
 
-    def kill_enemy(self):
+    def kill_enemy(self, base):
+        """Kills the enemy and calls reward_resources"""
         self._status = False
+        self.reward_resources(base)
 
     def reward_resources(self, base):
+        """Gives player resources based on how much the enemy is worth"""
         base.add_money(self._resource_worth)
 
     def damage_base(self, base):
+        """Damages the base and removes the enemy"""
         base.remove_health(self.get_enemy_strength())
         self._status = False
 
     def is_alive(self):
+        """Returns the enemy status"""
         return self._status
 
     def get_enemy_type(self):
+        """Returns the enemy type"""
         return self._type
 
     def get_enemy_health(self):
+        """Returns the enemy's health"""
         return self._health
 
     def get_enemy_speed(self):
+        """Returns the enemy's speed"""
         return self._speed
 
     def get_enemy_strength(self):
+        """Returns the enemy's strength"""
         return self._strength
 
     def render(self, window):
+        """Renders the enemy, currently as a circle. Plan to render as different shapes based on enemy type"""
         pygame.draw.circle(window, (255, 0, 0),
                            (int(self._position[0]), int(self._position[1])), 10)
-        self._draw_health_bar(window)
 
     def _draw_health_bar(self, window):
+        """Creates a visual for the player to know the status of the enemy's health"""
         health_ratio = self._health / self._max_health
+        """Finds ratio of health"""
         green = int(255 * health_ratio)
         red = 255 - green
-        pygame.draw.rect(window, (red, green, 0), (self._position[0] - 15, self._position[1] - 20, 30 * health_ratio, 5))
+        pygame.draw.rect(window, (red, green, 0),
+                         (self._position[0] - 15, self._position[1] - 20, 30 * health_ratio, 5))
--- waves.py
+++ waves.py
@@ -1,21 +1,28 @@
-import time
-from enemy import Enemy
-
-
 class Wave:
+    """The class used for generating waves"""
     def __init__(self, enemy_data: list, spawn_timer: int):
-        self._enemy_data = enemy_data  # Enemy data of current wave
+        self._enemy_data = enemy_data
+        """Enemy data of current wave"""
         self._spawn_timer = spawn_timer
-        self._enemy_list = []  # Enemies that have been spawned
+        """Time between the spawning of enemies"""
+        self._enemy_list = []
+        """Enemies that have been spawned"""
         self._enemy_number = 0
+        """Tracks which enemy is going to be spawned next"""
 
     def spawn_enemy(self):
+        """Function that spawns enemies"""
         if self._enemy_number < len(self._enemy_data):
+            """Checks if there are still more enemies to spawn"""
             this_enemy = self._enemy_data[self._enemy_number]
             self._enemy_list.append(this_enemy)
+            """Spawns the next enemy"""
             self._enemy_number += 1
+            """Prepares the next enemy to be spawned"""
             return this_enemy
         return None
+        """If there are no enemies to spawn, return None"""
 
     def _is_wave_complete(self):
+        """Checks if the wave is complete"""
         return self._enemy_number >= len(self._enemy_data) and all(not enemy.is_alive() for enemy in self._enemy_list)
